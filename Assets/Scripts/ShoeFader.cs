998a61c0adae23ffb794a4552b80853b12c75cdb
Xroft666 <vapyhtin@gmail.com>
2014-02-07 14:42:01 +0100

    footsteps in 5.SomeoneApproaches scene
    Dont know if will fito to the new background though

---
+++ Assets/Scripts/ShoeFader.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShoeFader : MonoBehaviour 
+{
+	public float startTime = 0f;
+//	public float fadeTime = 2f;
+
+	private bool fading = false;
+	private float fadeFrom = 1f;
+	public float fadeStep = 0.1f;
+
+	IEnumerator Start () {
+
+		yield return new WaitForSeconds( startTime );
+
+		renderer.material.SetColor ("_Color", new Color (1f, 1f, 1f, 1f));
+		fading = true;
+	}
+
+	void Update () {
+
+		if (! fading )
+			return;
+
+		fadeFrom -= fadeStep;
+
+		if( fadeFrom <= 0f )
+		{
+			fading = false;
+			renderer.material.SetColor ("_Color", new Color (1f, 1f, 1f, 0f));
+			return;
+
+		}
+
+		renderer.material.SetColor ("_Color", new Color (1f, 1f, 1f, fadeFrom));
+	}
+}
